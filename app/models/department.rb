5ca063989abfd9c66b3646ae5937398d15995315
Aaron Storrer <aaron.storrer@gmail.com>
2015-01-20 14:48:14 -0500

    Added Validations on Department

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,7 @@
 class Department < ActiveRecord::Base
   belongs_to :facility
 
+  validates :name, presence: true, length: { maximum: 45 }, uniqueness: true
+  validates :facility_id, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 1000 }
+
 end
