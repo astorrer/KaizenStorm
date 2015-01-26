edde34d19e5630a1836ba370d39e948827c32788
Aaron Storrer <aaron.storrer@gmail.com>
2015-01-26 08:37:36 -0500

    Validations

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -7,5 +7,10 @@ class Suggestion < ActiveRecord::Base
   scope :completed,  -> { where(completed: true) }
   scope :verified,   -> { where(verified: true) }
   scope :funded,     -> { where(funded: true) }
-
+  
+  validates :facility_id, presence: true
+  validates :department_id, presence: true
+  validates :current_state, presence: true
+  validates :ideal_state, presence: true
+  validates :benefits, presence: true
 end
